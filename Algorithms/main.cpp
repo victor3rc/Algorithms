1c1fccf3eb1cd1a3c757790310928c6818fdc2a7
Victor Cardozo <victor3rc@gmail.com>
2015-01-13 11:35:32 +0000

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Victor Cardozo on 13/01/2015.
+//  Copyright (c) 2015 Victor Cardozo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
